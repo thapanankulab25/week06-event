29a66656d3d193e9a2c559ef1e5ce3545091e8ec
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-01-15 10:44:37 +0700

    create class event-data

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,20 +1,27 @@
-// import React from 'react'
-// import Button from './class-components'
-// import { Calculator } from './calculator'
+import React from 'react'
+import Button from './class-components'
+import { Calculator } from './calculator'
+import { EventData2 } from './event-data'
 
 
-// function App(){
-//     return <Calculator/>
-// }
-// export default App
+export default function App(){
+    return <EventData2/>
 
-// export default function App(){
-//     return <EventData1/>
-
-import React from 'react';
-import Calculator2 from './calculator';
-
-export default function App() {
-  return <Calculator2 />;
 }
+
+//function
+// import React from 'react';
+// import Calculator2 from './calculator';
+
+// export default function App() {
+//   return <Calculator2 />;
+// }
+
+// class
+// import React from 'react';
+// import {Calculator} from './calculator';
+
+// export default function App() {
+//   return <Calculator />;
+// }
 
--- app1/src/event-data.js
+++ app1/src/event-data.js
@@ -1,27 +1,50 @@
 import React from 'react';
-
-export class EventData1 extends React.Component{
-    onClickStartStop = (ev) => {
+export function EventData2(){
+    const onClickStartStop = (ev) => {
         let t = ev.target.innerText
         let s = (t === 'Start') ? 'Stop': 'Start'
         ev.target.innerText = s
     }
 
-    onclickAdd = (ev) => {
+    const onClickAdd = (ev) => {
         let t = ev.target.innerText
         let r = eval(t)
         alert(`${t} = ${r}`)
     }
-    render(){
         return (
             <div style={{textAlign:'center',marginTop:20}}>
-                <button onClick={(ev) => this.onClickStartStop(ev)}>Start</button>
+                <button onClick={(ev) => onClickStartStop(ev)}>Start</button>
                 <br/><br/>
-                <button onClick={(ev) => this.onClickAdd(ev)}>10 + 20</button>
+                <button onClick={(ev) => onClickAdd(ev)}>10 + 20</button>
                 <br/><br/>
-                <button onClick={this.onClickAdd.bind(this)}>30 * 40</button>
+                <button onClick={(ev) => onClickAdd(ev)}>30 * 40</button>
                 <br/><br/>
             </div>
         )
-    }
 }
+
+// export class EventData1 extends React.Component{
+//     onClickStartStop = (ev) => {
+//         let t = ev.target.innerText
+//         let s = (t === 'Start') ? 'Stop': 'Start'
+//         ev.target.innerText = s
+//     }
+
+//     onclickAdd = (ev) => {
+//         let t = ev.target.innerText
+//         let r = eval(t)
+//         alert(`${t} = ${r}`)
+//     }
+//     render(){
+//         return (
+//             <div style={{textAlign:'center',marginTop:20}}>
+//                 <button onClick={(ev) => this.onClickStartStop(ev)}>Start</button>
+//                 <br/><br/>
+//                 <button onClick={(ev) => this.onClickAdd(ev)}>10 + 20</button>
+//                 <br/><br/>
+//                 <button onClick={this.onClickAdd.bind(this)}>30 * 40</button>
+//                 <br/><br/>
+//             </div>
+//         )
+//     }
+// }
