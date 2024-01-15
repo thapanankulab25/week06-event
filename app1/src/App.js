53f0dff40006d8f1f44ed0143943b0308b1740b6
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-01-15 10:36:10 +0700

    update function calculator

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Button from './class-components'
-import { Calculator } from './calculator'
-// import {EventData1} from './event-data'
+// import React from 'react'
+// import Button from './class-components'
+// import { Calculator } from './calculator'
 
-function App(){
-    return <Calculator/>
-}
-export default App
+
+// function App(){
+//     return <Calculator/>
+// }
+// export default App
 
 // export default function App(){
 //     return <EventData1/>
+
+import React from 'react';
+import Calculator2 from './calculator';
+
+export default function App() {
+  return <Calculator2 />;
+}
 
